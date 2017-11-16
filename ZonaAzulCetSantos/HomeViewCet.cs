5325b5ffc69f9cc6ab5a0d2128d0487287e273b9
gelvane nascimento silva <gelvane.silva@outlook.com>
2017-11-16 21:49:37 -0200

    inclusão de views cet

---
+++ ZonaAzulCetSantos/HomeViewCet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ZonaAzulCetSantos
+{
+    [Activity(Label = "HomeViewCet")]
+    public class HomeViewCet : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.HomeViewCet);
+            // Create your application here
+        }
+    }
+}
--- ZonaAzulCetSantos/Resources/Resource.Designer.cs
+++ ZonaAzulCetSantos/Resources/Resource.Designer.cs
@@ -106,7 +106,10 @@ namespace ZonaAzulCetSantos
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int LoginCet = 2130837504;
+			public const int HomeViewCet = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int LoginCet = 2130837505;
 			
 			static Layout()
 			{
